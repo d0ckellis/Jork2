0ad0e98015062e5de5cc991352fed819aabec961
alexandravuu <fujoshivu@gmail.com>
2022-03-03 15:50:01 -0800

    new input

--- src/Jork.java
+++ src/Jork.java
@@ -1,5 +1,9 @@
 public class Jork {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Coolio");
     }
+
 }
+
+
--- src/Player.java
+++ src/Player.java
@@ -2,7 +2,5 @@ public interface Player {
     //hi this is Ally
     //1, 2, 3, 4 testing testing
 
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+
 }
