e2ac8ffdc97f8eea06e3afe9a9382d4fa0e96758
d0ckellis <alexlynnblack@gmail.com>
2022-03-03 17:08:09 -0800

    removed test lines added while we were learning to use git

--- src/Jork.java
+++ src/Jork.java
@@ -1,9 +1,4 @@
 public class Jork {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        int n = 9;
-
-        System.out.println("Can you be a weeb if you're asian?");
-        System.out.println("idk bro");
     }
 }
