8fccf4cea070856bd024dff1ebc594fc4d0a219c
AndyHyukHan <mensahibiki@gmail.com>
2022-03-03 19:28:12 -0800

    Added basic framework to GameManager and Jork

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,2 +1,5 @@
+import java.util.Scanner;
+
 public class GameManager {
+    Scanner console = new Scanner(System.in); //
 }
--- src/Jork.java
+++ src/Jork.java
@@ -1,9 +1,14 @@
 public class Jork {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        int n = 9;
+        System.out.println("Starting Jork...");
 
-        System.out.println("Can you be a weeb if you're asian?");
-        System.out.println("idk bro");
+    }
+
+    public static void gameBuilder() {
+
+    }
+
+    public static void consoleWipe() {
+
     }
 }
