df422c1db0136a9abb4ffd8c489ee03aa4d6ce3d
AndyHyukHan <mensahibiki@gmail.com>
2022-03-03 20:25:32 -0800

    Moved gameBuilder and consoleWipe to Setup Added strings 'startUp', 'gameOver', 'namePrompt', and 'perkPrompt' to GameManager.

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,5 +1,11 @@
-import java.util.Scanner;
-
+/**
+ * A class to store the various strings to be printed into the console.
+ * @author andyh
+ * @version 1.0
+ */
 public class GameManager {
-    Scanner console = new Scanner(System.in); //
+    public String startUp = "Booting...\nWelcome to Jork!\nA text-based Role-Playing game where you choose your journey!";
+    public String gameOver = "Your journey has no come to an end...";
+    public String namePrompt = "What's our hero's name?";
+    public String perkPrompt = "";
 }
--- src/Setup.java
+++ src/Setup.java
@@ -1,2 +1,17 @@
 public class Setup {
+    // public MapBuilder mapBuilder = new MapBuilder();
+
+    /**
+     *
+     */
+    public static void gameBuilder() {
+
+    }
+
+    /**
+     * Empties the visible {@code Console} after every action
+     */
+    public static void consoleWipe() {
+
+    }
 }
