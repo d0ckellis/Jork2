bc2b075471a18e195f8a080b2e360d77e91a9913
jaspersn <jasper.s.newkirk@gmail.comgit>
2022-03-03 16:04:55 -0800

    Basic Item enumeration implementation

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,49 @@
+/**
+ * An enumeration for items to be stored in the {@link Inventory#inventory} {@code HashMap}.
+ * @author jaspesn
+ * @version 1.0
+ */
 public enum Item {
+
+    /**
+     * Sword
+     */
+    SWORD("SWORD"),
+    /**
+     * Rug
+     */
+    RUG("RUG"),
+    /**
+     * Nails
+     */
+    NAILS("NAILS"),
+    /**
+     * Potion
+     */
+    POTION("POTION"),
+    /**
+     * Key
+     */
+    KEY("KEY");
+
+    /**
+     * The {@code String} corresponding to a particular value of the enumeration.
+     */
+    private final String key;
+
+    /**
+     * Constructs a new {@link Item} with the specified {@code String}.
+     * @param key The {@code String}.
+     */
+    Item(final String key) {
+        this.key = key;
+    }
+
+    /**
+     * Returns the {@code String} corresponding to a particular value of the enumeration.
+     * @return the {@code String} corresponding to a particular value of the enumeration.
+     */
+    public String getKey() {
+        return key;
+    }
 }
