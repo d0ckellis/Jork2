76ff5906800244a6f326d1ca1f2e7f371f318661
alexandravuu <fujoshivu@gmail.com>
2022-03-03 20:27:35 -0800

    Player Class alteration

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,32 @@
 public class Player {
-    //hi this is Ally
-    //1, 2, 3, 4 testing testing
-     private Inventory inventory;
+    String name;
+    int power;
+
+    //creating getter and setter for name and power of player
+    public void setName(String n) {
+        name = n;
+
+        System.out.println ("Player 1 set your name: ");
+
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public void setPower(int p) {
+        power = p;
+    }
+    public int getPower() {
+        return power;
+    }
+
+
+
+    private Inventory inventory;
+
+
 
 
 
