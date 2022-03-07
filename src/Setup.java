8f3b1ca2279a133ee871033e18878ad5c9e86951
Andy <mensahibiki@gmail.com>
2022-03-06 22:25:29 -0800

    Added booleans startGame and endGame and an int gameCh in Setup for use when building our game. Added Scanner console to gameBuilder for later use.

--- src/Setup.java
+++ src/Setup.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+/**
+ * A class for our {@link Setup#gameBuilder} and {@link Setup#consoleWipe}
+ * @author andyh
+ * @version 1.0
+ */
 public class Setup {
+    /**
+     * A boolean responsible for determining whether our game has started
+     */
+    private boolean startGame;
+    /**
+     * A boolean responsible for determining if the game has ended
+     */
+    private boolean endGame;
+    /**
+     * An int that holds the chapter number to play
+     */
+    private int gameCh;
 
     // public MapBuilder mapBuilder = new MapBuilder();
 
     /**
-     *
+     * A builder class for our game
      */
-    public static void gameBuilder() {
+    public static void gameBuilder(Scanner console) {
 
     }
 
