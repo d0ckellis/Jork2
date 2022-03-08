1113bc09ddc1911cdb1cc5119d8f713aa74d045f
d0ckellis <alexlynnblack@gmail.com>
2022-03-08 15:22:07 -0800

    deleted mapBuilder

--- src/Map.java
+++ src/Map.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
  * @version 1.2
  */
 public class Map {
+    private final Scanner console;
     private final int startPosition;
     private final int width;
     private final int height;
@@ -13,6 +14,7 @@ public class Map {
     Space[] map;
 
     private Map(Builder builder) {
+        this.console = builder.console;
         this.startPosition = builder.startPosition;
         this.width = builder.width;
         this.height = builder.height;
@@ -28,12 +30,14 @@ public class Map {
     public int getCurPosition() {return curPosition;}
 
     public static class Builder {
+        private final Scanner console;
         private final int startPosition;
         private final int width;
         private final int height;
         Space[] map;
 
-        public Builder (int startPosition, int width, int height, Space[] map) {
+        public Builder (Scanner console, int startPosition, int width, int height, Space[] map) {
+            this.console = console;
             this.startPosition = startPosition;
             this.width = width;
             this.height = height;
@@ -41,6 +45,32 @@ public class Map {
         }
     }
     public void move() {
+        System.out.println("\nWhich direction would you like to move? ");
+        String direction = console.next();
+        console.nextLine();
+            switch(direction.toUpperCase()) {
+                case "NORTH":
+                case "N":
+                case "UP":
+                    moveUp();
+                    break;
+                case "SOUTH":
+                case "S":
+                case "DOWN":
+                    moveDown();
+                    break;
+                case "EAST":
+                case "E":
+                case "RIGHT":
+                    moveRight();
+                    break;
+                case "WEST":
+                case "W":
+                case "LEFT":
+                    moveLeft();
+                    break;
+
+            }
     }
     public int moveRight() {
         if (curPosition + 1 % width == 0) invalidMove();
