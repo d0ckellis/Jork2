4bc5cccc2fedfcab76a935fe188082ef9ce1547d
alexandravuu <fujoshivu@gmail.com>
2022-03-08 16:19:42 -0800

    Description class (skeleton only)

---
+++ src/Description.java
@@ -0,0 +1,31 @@
+public class Description {
+    String DescriptSpace0Item = "";
+    String DescriptSpace0NoItem = "";
+
+    String DescriptSpace1Item = "";
+    String DescriptSpace1NoItem = "";
+
+    String DescriptSpace2Item = "";
+    String DescriptSpace2NoItem = "";
+
+    String DescriptSpace3Item = "";
+    String DescriptSpace3NoItem = "";
+
+    String DescriptSpace4Item = "";
+    String DescriptSpace4NoItem = "";
+
+    String DescriptSpace5Item = "";
+    String DescriptSpace5NoItem = "";
+
+    String DescriptSpace6Item = "";
+    String DescriptSpace6NoItem = "";
+
+    String DescriptSpace7Item = "";
+    String DescriptSpace7NoItem = "";
+
+    String DescriptSpace8Item = "";
+    String DescriptSpace8NoItem = "";
+
+    String DescriptSpace9Item = "";
+    String DescriptSpace9NoItem = "";
+}
