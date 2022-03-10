77f2ef03d10f1784971d0eec85447f4dda4e3fa2
alexandravuu <fujoshivu@gmail.com>
2022-03-09 19:42:38 -0800

    Description

--- src/Description.java
+++ src/Description.java
@@ -1,31 +1,34 @@
 public class Description {
-    String.ch1 DescriptSpace0Item = "";
-    String.ch1 DescriptSpace0NoItem = "";
+    String DescriptSpace0NailCh1= "\tYour bed awaits your fated return.";
+    String DescriptSpace0NoNailCh1 = "\\tYou stub your toe on two nails protruding from the floorboards,\\n\" +\n" +
+            "            \"\\tDid you do this last night too or is it just deja vu?\\n\" +\n" +
+            "            \"\\tIt seems you're doomed to keep repeating this misfortune.\\n\" +\n" +
+            "            \"\\tYour bed awaits your fated return.";
 
-    String DescriptSpace1Item = "";
+    String DescriptSpace1ItemCh1 = "";
     String DescriptSpace1NoItem = "";
 
-    String DescriptSpace2Item = "";
-    String DescriptSpace2NoItem = "";
+    String DescriptSpace2ItemCh1 = "";
+    String DescriptSpace2NoItemCh1 = "";
 
-    String DescriptSpace3Item = "";
-    String DescriptSpace3NoItem = "";
+    String DescriptSpace3ItemCh1 = "";
+    String DescriptSpace3NoItemCh1 = "";
 
-    String DescriptSpace4Item = "";
-    String DescriptSpace4NoItem = "";
+    String DescriptSpace4ItemCh1 = "";
+    String DescriptSpace4NoItemCh1 = "";
 
-    String DescriptSpace5Item = "";
-    String DescriptSpace5NoItem = "";
+    String DescriptSpace5ItemCh1 = "";
+    String DescriptSpace5NoItemCh1 = "";
 
-    String DescriptSpace6Item = "";
-    String DescriptSpace6NoItem = "";
+    String DescriptSpace6ItemCh1 = "";
+    String DescriptSpace6NoItemCh1 = "";
 
-    String DescriptSpace7Item = "";
-    String DescriptSpace7NoItem = "";
+    String DescriptSpace7ItemCh1 = "";
+    String DescriptSpace7NoItemCh1 = "";
 
-    String DescriptSpace8Item = "";
-    String DescriptSpace8NoItem = "";
+    String DescriptSpace8ItemCh1 = "";
+    String DescriptSpace8NoItemCh1 = "";
 
-    String DescriptSpace9Item = "";
-    String DescriptSpace9NoItem = "";
+    String DescriptSpace9ItemCh1 = "";
+    String DescriptSpace9NoItemCh1 = "";
 }
